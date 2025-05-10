4d2722dcf53fd2a099cf7b1370561e1feb04cbf9
danielnjama <danielnjama2015@gmail.com>
2025-05-10 11:44:16 +0300

    url rectified

--- certapp/urls.py
+++ certapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path('download-certificate/<int:enrolment_id>/', views.download_certificate, name='download_certificate'),
     path("",views.index,name="index"),
-    path('verify-certificate/<str:code>/', views.verify_certificate, name='verify_certificate')
+    path('cert/<str:code>/', views.verify_certificate, name='verify_certificate')
 
     
 ]
